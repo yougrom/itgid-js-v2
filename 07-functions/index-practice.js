1404286c77fcbb1da1e9296d1353eb1d75bf3fd6
Herr Gromov <youriy.gromov@gmail.com>
2024-02-27 19:41:05 +0100

    functions practice

---
+++ 07-functions/index-practice.js
@@ -0,0 +1,24 @@
+function greet() {
+return "hello world!";
+};
+// Example 2
+function boolToWord( bool ){
+if (bool) {
+    return "Yes";
+} else {
+    return "No";
+}
+}
+boolToWord(10);
+
+// Example 3
+function greet(name){
+    return `Hello, ${name} how are you doing today?`;
+}
+
+// Example 4
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+
+    return i * 2;
+  }
