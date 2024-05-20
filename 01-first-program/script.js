66903c963055670256bf8c746c6787123f1ef33a
Herr Gromov <youriy.gromov@gmail.com>
2024-05-20 12:46:25 +0200

    First program + Practice

--- 01-first-program/script.js
+++ 01-first-program/script.js
@@ -1,24 +1,23 @@
-'use strict';
-let helloWorld = 'Hello World';
+let helloWorld = "Hello World";
 console.log(helloWorld);
 
 // alert('Hello');
 
-document.getElementById('out').innerHTML = 'Hello';
-console.log(document.getElementById('out'));
+document.getElementById("out").innerHTML = "Hello";
+console.log(document.getElementById("out"));
 
-document.getElementById('out').innerHTML = '<h1>2024</h1>';
+document.getElementById("out").innerHTML = "<h1>2024</h1>";
 
-document.querySelector('h2.header').innerHTML = !500;
+document.querySelector("h2.header").innerHTML = 666;
 
-document.querySelector('#one').innerHTML = 'New Year';
+document.querySelector("#one").innerHTML = "New Year";
 
 let a; // объявление переменной
-a = document.querySelector('#one'); // присвоение переменной
+a = document.querySelector("#one"); // присвоение переменной
 
-let b = document.querySelector('#one'); // переменная дает возможность манипулировать элементом не прописывая каждый раз путь к нему.
+let b = document.querySelector("#one"); // переменная дает возможность манипулировать элементом не прописывая каждый раз путь к нему.
 
-b.innerHTML = 'WOW!';
+b.innerHTML = "WOW!";
 
 let c = 7;
 let d = 9;
@@ -26,8 +25,6 @@ console.log(c * d);
 
 console.log(helloWorld);
 
-
-
 {
-let a = 6;
+  let a = 6;
 }
