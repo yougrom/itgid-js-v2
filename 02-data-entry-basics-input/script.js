b1b361ca6f5234163bd9a28e2064722e88908b20
Herr Gromov <youriy.gromov@gmail.com>
2024-05-21 13:21:56 +0200

    Data Entry Basics + Practice

--- 02-data-entry-basics-input/script.js
+++ 02-data-entry-basics-input/script.js
@@ -1,30 +1,28 @@
-'use strict';
 // let a = 6;
 // let b = 'Hello';
 // console.log(a);
 
 // Example
 // document.querySelector('button').innerHTML = 'Hello';
-let inputIn = document.querySelector('.input-in'); // input
+let inputIn = document.querySelector(".input-in"); // input
 
-
-let button =  document.querySelector('button'); // button
-let div = document.querySelector('.out'); // 
-let divNew = document.querySelector('.out-new');
+let button = document.querySelector("button"); // button
+let div = document.querySelector(".out"); //
+let divNew = document.querySelector(".out-new");
 
 // эта функция выполнится только при нажатии на кнопку
 button.onclick = function () {
-    
-    console.log('Work!!');
-    let b = +inputIn.value; // ставим плюс если нам нужно выводить числа, + позволяет перевести в число
-    console.log(b); // то что введено в инрут
-    console.log(b * 2); // NaN - если в поле value ввели String
-    console.log(b / 0); // infinity
-    console.log(b + 10); //
-    div.innerHTML = b; // b - то что было введено в Input
-    divNew.innerHTML = b;
-    inputIn.value = '';
-    console.log(inputIn);
+  console.log(inputIn.value);
+  console.log("Work!!");
+  let b = +inputIn.value; // ставим плюс если нам нужно выводить числа, + позволяет перевести в число
+  console.log(b); // то что введено в инрут
+  console.log(b * 2); // NaN - если в поле value ввели String
+  console.log(b / 0); // infinity
+  console.log(b + 10); //
+  div.innerHTML = b; // b - то что было введено в Input
+  divNew.innerHTML = b;
+  inputIn.value = "";
+  console.log(inputIn);
 };
 
 // onclick указывает некоторый сценарий, который будет запускаться при щелчке по элементу.
