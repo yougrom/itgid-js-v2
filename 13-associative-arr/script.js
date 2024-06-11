1c2fc1d2210d499e78a3aca7a7ff2802962db8c9
Herr Gromov <youriy.gromov@gmail.com>
2024-06-11 15:26:06 +0200

    Object + Practice

--- 13-associative-arr/script.js
+++ 13-associative-arr/script.js
@@ -1,11 +1,11 @@
 const a = {
-  "a": 1,
-  "b": 'Hello',
-  "c": true,
-  "d": [1, 2, 3],
-  "e": {
-    "name": 'Alex',
-    "age": 25
+  a: 1,
+  b: 'Hello',
+  c: true,
+  d: [1, 2, 3],
+  e: {
+    name: 'Alex',
+    age: 25,
   },
   y43: 1999,
   'villa de': 3024,
@@ -24,7 +24,7 @@ a.c = false; // Изменение значения элемента
 // Как удалить значение элемента в массиве
 delete a.b; // Удаление элемента
 console.log(a);
-console.log(a["a"]);
+console.log(a['a']);
 console.log(a.e);
 
 let k = 'b';
@@ -32,11 +32,11 @@ console.log(a[k]); // Hello
 
 // document.querySelector('.out').innerHTML = a; — такой способ вывода ассоциативного массива не подходит
 
-console.log(a["e"]["name"]); // Alex
+console.log(a['e']['name']); // Alex
 
 let out = '';
 for (let key in a) {
-  out += key + ' = ' + a[key] +'<br>'; // Вывод всех Ключ и значений массива
+  out += key + ' = ' + a[key] + '<br>'; // Вывод всех Ключ и значений массива
 }
 document.querySelector('.out').innerHTML = out;
 
@@ -46,7 +46,7 @@ function filterRange(arr, a, b) {
   let out = [];
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] >= a && arr[i] <= b) {
-     out.push(arr[i]) ;
+      out.push(arr[i]);
     }
   }
   return out;
@@ -54,30 +54,24 @@ function filterRange(arr, a, b) {
 
 let filtered = filterRange(arr, 1, 4);
 
-console.log( filtered ); // 3,1 (совпадающие значения)
+console.log(filtered); // 3,1 (совпадающие значения)
 
-console.log( arr ); // 5,3,8,1 (без изменений)
-
+console.log(arr); // 5,3,8,1 (без изменений)
 
 // Task
 let arr1 = [5, 2, 1, -10, 8];
 arr1.sort().reverse().reverse().reverse();
 // ... ваш код для сортировки по убыванию
 
-
-console.log( arr1 ); // 8, 5, 2, 1, -10
-
-
-
-
+console.log(arr1); // 8, 5, 2, 1, -10
 
 const items = [
-  { name: "Edward", value: 21 },
-  { name: "Sharpe", value: 37 },
-  { name: "And", value: 45 },
-  { name: "The", value: -12 },
-  { name: "Magnetic", value: 13 },
-  { name: "Zeros", value: 37 },
+  { name: 'Edward', value: 21 },
+  { name: 'Sharpe', value: 37 },
+  { name: 'And', value: 45 },
+  { name: 'The', value: -12 },
+  { name: 'Magnetic', value: 13 },
+  { name: 'Zeros', value: 37 },
 ];
 
 // sort by value
@@ -99,6 +93,3 @@ const abc = items.sort((a, b) => a.value - b.value).reverse();
 // });
 
 console.log(abc);
-
-
-
