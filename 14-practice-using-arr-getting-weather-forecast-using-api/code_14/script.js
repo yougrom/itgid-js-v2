fd09c93be7801b2b157dee580152c505d37e3a4e
Herr Gromov <youriy.gromov@gmail.com>
2024-06-13 21:52:12 +0200

    Practice using arr getting weather forecast using API

--- 14-practice-using-arr-getting-weather-forecast-using-api/code_14/script.js
+++ 14-practice-using-arr-getting-weather-forecast-using-api/code_14/script.js
@@ -1,21 +1,28 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?id=703448&appid=a316cc86e8b0b6b7394f561f97e066aa')
-    .then(function (resp) { return resp.json() })
-    .then(function (data) {
-        console.log(data);
-        document.querySelector('.package-name').textContent = data.name;
-        document.querySelector('.price').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
-        document.querySelector('.disclaimer').textContent = data.weather[0]['description'];
-        //https://openweathermap.org/img/wn/02d@2x.png
-        document.querySelector('.features li').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
-    })
-    .catch(function () {
-        // catch any errors
-    });
+fetch(
+  'https://api.openweathermap.org/data/2.5/weather?id=703448&appid=a316cc86e8b0b6b7394f561f97e066aa'
+)
+  .then(function (resp) {
+    return resp.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    document.querySelector('.package-name').textContent = data.name;
+    document.querySelector('.price').innerHTML =
+      Math.round(data.main.temp - 273) + '&deg;';
+    document.querySelector('.disclaimer').textContent =
+      data.weather[0]['description'];
+    //https://openweathermap.org/img/wn/02d@2x.png
+    document.querySelector(
+      '.features li'
+    ).innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+  })
+  .catch(function () {
+    // catch any errors
+  });
 
-    // https://api.openweathermap.org/data/2.5/weather
-    // ?q=Berlin&appid=a316cc86e8b0b6b7394f561f97e066aa — GET Запрос
-    // & - Разделитель параметров
-    // GET Параметры используется для того чтобы конфигурировать GET запрос.
+// https://api.openweathermap.org/data/2.5/weather
+// ?q=Berlin&appid=a316cc86e8b0b6b7394f561f97e066aa — GET Запрос
+// & - Разделитель параметров
+// GET Параметры используется для того чтобы конфигурировать GET запрос.
 
-    // fetch - Это интерфейс джаваскрипт для работы с запросами и ответами HTTP
-
+// fetch - Это интерфейс джаваскрипт для работы с запросами и ответами HTTP
