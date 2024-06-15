baf36c967c7f5846c6f59bc5e15fa71f6d515378
Herr Gromov <youriy.gromov@gmail.com>
2024-06-15 16:00:28 +0200

    Set + Practice

--- 15-set-in-js/script.js
+++ 15-set-in-js/script.js
@@ -1,8 +1,7 @@
 // // Set - Это новый объекты которые могут содержать только уникальное значение
-
 const a = new Set([1, 2, 3, 'Hello']);
 a.add(4);
-a.add('Hello'); 
+a.add('Hello');
 a.add(1); // Сайт устанавливает коллекцию уникальных элементов, Единица в данном случае не добавится в коллекцию set
 a.add(5);
 a.add('2'); // === Проверка проводится не только по значению а по типу
@@ -19,7 +18,7 @@ console.log(a.has('5')); // false
 
 for (let i of a) {
   console.log(i);
-};
+}
 
 let arr = [1, 2, 3, 4, 5, 'Hi', 6, 7, 8];
 let b = new Set(arr);
@@ -30,21 +29,37 @@ let setToArr = Array.from(b);
 console.log(setToArr);
 console.log(setToArr[3]);
 
-
 // Task
-const myArry = [true,  true,  true,  false,
-  true,  true,  true,  true ,
-  true,  false, true,  false,
-  true,  false, false, true ,
-  true,  true,  true,  true ,
-  false, false, true,  true];
+const myArry = [
+  true,
+  true,
+  true,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  true,
+  false,
+  true,
+  false,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  false,
+  true,
+  true,
+];
 
 const countSheeps = (sheep) => {
-  const filterResult = sheep.filter(element => element === true);
+  const filterResult = sheep.filter((element) => element === true);
   return filterResult.length;
-}
+};
 
 console.log(countSheeps(myArry));
-
-
-
